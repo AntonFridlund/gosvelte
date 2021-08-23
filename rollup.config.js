4f409eba28e6c1a4bbe19c39bc85350cfc109a31
AntonFridlund <anton.fridlund@mail.com>
2021-08-23 02:19:21 +0200

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,48 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
+import copy from 'rollup-plugin-copy'
 import css from 'rollup-plugin-css-only';
 
+// In and outputs.
+let baseInput = 'src/views/'
+let baseOutput = 'src/build/'
+let fileNameInput = 'main.js'
+let fileNameOutput = 'bundle.js'
+
+// Pages to be built.
+// Names must be unique.
+let inputs = ['home', 'profile']
+
+// Default plugins that will be run for every page.
 let defaultPlugins = [
 	svelte(),
-	css({output:'bundle.css'}),
-	resolve({browser:true, dedupe:['svelte']}),
+	css({ output: 'bundle.css' }),
+	resolve({ browser: true, dedupe: ['svelte'] }),
 	terser()
 ]
 
-export default [{
-	input: 'src/views/home/main.js',
-	output: {
-		sourcemap: false,
-		format: 'iife',
-		name: 'home',
-		file: 'src/build/home/bundle.js'
-	},
-	plugins: defaultPlugins
-},
-{
-	input: 'src/views/profile/main.js',
-	output: {
-		sourcemap: false,
-		format: 'iife',
-		name: 'profile',
-		file: 'src/build/profile/bundle.js'
-	},
-	plugins: defaultPlugins
-}]
+let defaultExport = []
+
+// Generate a list of every page config.
+for (let i = inputs.length; i--;) {
+	defaultExport[defaultExport.length] = {
+		input: baseInput + inputs[i] + '/' + fileNameInput,
+		output: {
+			sourcemap: false,
+			format: 'iife',
+			name: inputs[i],
+			file: baseOutput + inputs[i] + '/' + fileNameOutput
+		},
+		plugins: [
+			...defaultPlugins,
+			copy({ targets: [{
+				src: baseOutput + 'template.html',
+				dest: baseOutput + inputs[i],
+				rename: 'index.html'
+			}]})
+		]
+	}
+}
+
+export default defaultExport
