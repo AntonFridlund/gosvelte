d70c1b40e589faff8fa034972f9d6a3e005b3f6f
AntonFridlund <anton.fridlund@mail.com>
2021-08-23 02:20:13 +0200

    Add path prefix

--- src/controllers/home/home_controller.go
+++ src/controllers/home/home_controller.go
@@ -3,8 +3,7 @@ package user
 import "github.com/gofiber/fiber/v2"
 
 func Home(c *fiber.Ctx) error {
-	return c.Render("index", fiber.Map{
-		"Path":  "home",
-		"Title": "Home page",
+	return c.Render("home/index", fiber.Map{
+		"Path": "home",
 	})
 }
--- src/controllers/profile/profile_controller.go
+++ src/controllers/profile/profile_controller.go
@@ -3,8 +3,7 @@ package user
 import "github.com/gofiber/fiber/v2"
 
 func Profile(c *fiber.Ctx) error {
-	return c.Render("index", fiber.Map{
-		"Path":  "profile",
-		"Title": "Profile page",
+	return c.Render("profile/index", fiber.Map{
+		"Path": "profile",
 	})
 }
